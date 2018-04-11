50bbf5e08eebafb0c467d3e614b5a1a85d9eb42f
Adam Faulkner <adamfaulkner@Adams-MacBook-Pro.local>
2018-04-11 20:49:20 +0100

    keys is no longer commitable

---
+++ server/config/keys.js
@@ -0,0 +1,18 @@
+const moment = require('moment');
+
+const redirectDomain = 'http://localhost:5000';
+
+module.exports = {
+  googleClientID:
+    '767944435429-gch4ffs941ufr60ilqi28fcom6lmji8l.apps.googleusercontent.com',
+  googleClientSecret: 'UPDlPxTX28vp4lOA-s6n6IqH',
+  mongoURI: 'mongodb://adam:password@emaily-mongo:27017/emaily-dev',
+  jwtSecret: 'shhhhhhhhhhhhh',
+  jwtExpires: moment().add(1, 'hour'),
+  stripePublishableKey: 'pk_test_SBNcVd44pGOtY75NFWAJcJIG',
+  stripeSecretKey: 'sk_test_EXy10pE2W3CHYGjLB6KQOucE',
+  sendGridKey:
+    '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',
+  redirectDomain: redirectDomain,
+  thanksRedirect: redirectDomain + '/api/survey'
+};
